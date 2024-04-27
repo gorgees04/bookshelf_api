dfb328b4772dd4eb810341af7a428ffeb014505f
Gorgees Odisho <gorgees041@gmail.com>
2024-04-28 02:29:45 +0300

    add swagger config and export it in root route #15

--- server/server.js
+++ server/server.js
@@ -5,11 +5,17 @@ const db = require("./db");
 const PORT = process.env.SERVER_PORT || 3000;
 const routes = require("./routes/routes");
 const cookieParser = require("cookie-parser");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swagger = require("./swagger");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// swagger
+swagger(app);
 
 // routes
 app.use("/api", routes);
