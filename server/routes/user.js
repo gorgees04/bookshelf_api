02ee439a4d4ac4e501e2cf796c0357d5e922c6ae
Gorgees Odisho <gorgees041@gmail.com>
2024-04-27 04:35:15 +0300

    create user's routes #11

---
+++ server/routes/user.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const authUser = require("../middlewares/authUser");
+
+// decode and authenticate the user
+router.use(authUser);
+
+router.get("/", userController.getUserInfo);
+router.put("/", userController.updateUserInfo);
+router.delete("/", userController.deleteUserInfo);
+
+module.exports = router;
