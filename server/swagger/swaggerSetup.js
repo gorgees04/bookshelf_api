55498937db409440ce3ed7c1bc4a09dc04e7f6f2
Gorgees Odisho <gorgees041@gmail.com>
2024-04-28 20:42:28 +0300

    user add a new book endpoint doc

--- server/swagger/swaggerSetup.js
+++ server/swagger/swaggerSetup.js
@@ -13,5 +13,43 @@ const options = {
 const specs = swaggerJsDoc(options);
 
 module.exports = (app) => {
-  app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(specs));
+  // Customize Swagger UI configuration
+  const swaggerUIOptions = {
+    swaggerOptions: {
+      // Add support for file upload in request parameters
+      plugins: [
+        {
+          // Use a plugin to customize Swagger UI
+          Upload: function (system) {
+            return {
+              wrapComponents: {
+                parameters:
+                  (Original, { React }) =>
+                  (props) => {
+                    if (props.schema && props.schema.format === "binary") {
+                      // Add a file upload input for parameters of type 'file'
+                      return `
+                      <div class="form-group">
+                        <label for="${props.name}">${props.name}</label>
+                        <input type="file" id="${props.name}" name="${props.name}" onchange="handleFileChange(event)">
+                      </div>
+                    `;
+                    }
+                    // Render the original parameter component for other types
+                    return Original(props);
+                  },
+              },
+            };
+          },
+        },
+      ],
+    },
+  };
+
+  // Set up Swagger UI with custom options
+  app.use(
+    "/api-doc",
+    swaggerUI.serve,
+    swaggerUI.setup(specs, swaggerUIOptions)
+  );
 };
